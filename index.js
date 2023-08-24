426f635eeb3434a4d2c1ee3f15ab387b3a8847aa
benson <benson95217@gmail.com>
2023-08-24 15:18:34 +0800

    fix: 修改引入套件方式

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const express = require('express')
+// const express = require('express')
+import express from "express";
 
-const app = express()
-const PORT = 4000
+const app = express();
+const PORT = 4000;
 
 app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+  console.log(`API listening on PORT ${PORT} `);
+});
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
+app.get("/about", (req, res) => {
+  res.send("This is my about route..... ");
+});
 
 // Export the Express API
-module.exports = app
+module.exports = app;
